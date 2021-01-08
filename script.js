943c428e7900a3e9b3fd16dcdea48c9581b6ba98
Luis Cardona <cardona-luis@outlook.com>
2021-01-08 04:57:35 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,132 @@
+//Funtions for showing indivualt items.
+
+//kitchen
+function kitchenServices() {
+    document.getElementById("allServices").style.display = "none";
+    document.getElementById("kitchenItem").style.display = "grid";
+    document.getElementById("listOfAllServices").style.display = "grid";
+
+
+}
+
+function hideKitchen() {
+    document.getElementById("allServices").style.display = "grid";
+    document.getElementById("kitchenItem").style.display = "none";
+    document.getElementById("listOfAllServices").style.display = "none";
+}
+
+
+
+
+//bathroom
+function bathroomServices() {
+    document.getElementById("allServices").style.display = "none";
+    document.getElementById("bathroom").style.display = "grid";
+    document.getElementById("listOfAllServices").style.display = "grid";
+
+}
+
+function hideBathroom() {
+    document.getElementById("allServices").style.display = "grid";
+    document.getElementById("bathroom").style.display = "none";
+    document.getElementById("listOfAllServices").style.display = "none";
+}
+
+
+
+//Fireplace
+function fireplaceServices() {
+    document.getElementById("allServices").style.display = "none";
+    document.getElementById("fireplace").style.display = "grid";
+    document.getElementById("listOfAllServices").style.display = "grid";
+
+}
+
+function hidefireplace() {
+    document.getElementById("allServices").style.display = "grid";
+    document.getElementById("fireplace").style.display = "none";
+    document.getElementById("listOfAllServices").style.display = "none";
+}
+
+
+
+
+//Flooring
+function flooringServices() {
+    document.getElementById("allServices").style.display = "none";
+    document.getElementById("flooring").style.display = "grid";
+    document.getElementById("listOfAllServices").style.display = "grid";
+
+}
+
+function hideflooring() {
+    document.getElementById("allServices").style.display = "grid";
+    document.getElementById("flooring").style.display = "none";
+    document.getElementById("listOfAllServices").style.display = "none";
+}
+
+
+
+
+
+
+//Painting
+function paintingServices() {
+    document.getElementById("allServices").style.display = "none";
+    document.getElementById("painting").style.display = "grid";
+    document.getElementById("listOfAllServices").style.display = "grid";
+
+}
+
+function hidePainting() {
+    document.getElementById("allServices").style.display = "grid";
+    document.getElementById("painting").style.display = "none";
+    document.getElementById("listOfAllServices").style.display = "none";
+}
+
+
+
+//Doors and windows
+function doorsWindowsServices() {
+    document.getElementById("allServices").style.display = "none";
+    document.getElementById("doorsWindows").style.display = "grid";
+    document.getElementById("listOfAllServices").style.display = "grid";
+
+}
+
+function hideDoorsWindows() {
+    document.getElementById("allServices").style.display = "grid";
+    document.getElementById("doorsWindows").style.display = "none";
+    document.getElementById("listOfAllServices").style.display = "none";
+}
+
+
+
+//Room Addition
+function roomAdditionServices() {
+    document.getElementById("allServices").style.display = "none";
+    document.getElementById("roomAddition").style.display = "grid";
+    document.getElementById("listOfAllServices").style.display = "grid";
+
+}
+
+function hideRoomAddition() {
+    document.getElementById("allServices").style.display = "grid";
+    document.getElementById("roomAddition").style.display = "none";
+    document.getElementById("listOfAllServices").style.display = "none";
+}
+
+
+//outdoor Space
+function outdoorSpaceServices() {
+    document.getElementById("allServices").style.display = "none";
+    document.getElementById("outdoorSpace").style.display = "grid";
+    document.getElementById("listOfAllServices").style.display = "grid";
+
+}
+
+function hideOutdoorSpace() {
+    document.getElementById("allServices").style.display = "grid";
+    document.getElementById("outdoorSpace").style.display = "none";
+    document.getElementById("listOfAllServices").style.display = "none";
+}
